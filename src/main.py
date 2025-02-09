0b9c2e26cb03a42617ffb69838d4612185b832f2
petruskadominik <petruskadominik@gmail.com>
2025-02-09 13:48:00 +0100

    Testing deleting and recreating public dir

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 
 from block_to_html import markdown_to_html_node
 import os
+import shutil
 
 def main():
-    a = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(a)
+    base_dir = "/home/domino/workspace/github.com/petruskadominik/Staticsite"
+    public_dir = os.path.join(base_dir, "public")
+    static_dir = os.path.join(base_dir, "static")
+    
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir)
+
+    os.mkdir(public_dir)
+
+    shutil.copy(f"{static_dir}/*", public_dir)
 
 main()
 
